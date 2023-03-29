6be0de486de89c413e46eb7b363326b34d7ee124
AnxinYang <32281036+AnxinYang@users.noreply.github.com>
2023-03-28 22:53:45 -0700

    adding the summary api

--- server/main.ts
+++ server/main.ts
@@ -50,6 +50,44 @@ router.post("/api/chat", async (ctx) => {
   }
 });
 
+router.post("/api/summary", async (ctx) => {
+  const { messages }: { messages: Message[] } = await ctx.request.body().value;
+  try {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Deno.env.get("OPENAI_API_KEY")}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages,
+        max_tokens: +(Deno.env.get("MAX_MEMORY") || 500),
+        stream: false,
+      }),
+    };
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      requestOptions
+    );
+
+    if (!response.body) throw new Error("No response body");
+
+    // Send back the data (in chunks) as it comes in
+    ctx.response.headers.set("Content-Type", "application/json");
+    ctx.response.headers.set("Cache-Control", "no-cache");
+    ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+    ctx.response.headers.set("Access-Control-Allow-Methods", "*");
+    ctx.response.headers.set("allow", "*");
+    ctx.response.status = 200;
+    ctx.response.body = response.body;
+  } catch (error) {
+    console.error(error);
+    ctx.response.status = 500;
+    ctx.response.body = { error: `Error processing request.` };
+  }
+});
+
 // Create a new application
 const app = new Application();
 app.use(oakCors());
